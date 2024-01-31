591c373ecfa04ce3dba92a44f5f3a34d36eb2767
rohan7745 <138517784+rohan7745@users.noreply.github.com>
2024-01-31 20:53:01 +0000

    jan-fourth

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,32 @@
+// App.js
+import React from 'react';
 
-import React from "react";
-import './../styles/App.css';
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      temperature: 25,
+      conditions: 'Sunny',
+      threshold: 20,
+    };
+  }
 
-const App = () => {
-  return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
+  render() {
+    const { temperature, conditions, threshold } = this.state;
+
+    // Define the style based on the temperature and threshold
+    const temperatureStyle = {
+      color: temperature > threshold ? 'red' : 'blue',
+    };
+
+    return (
+      <div>
+        <h1>Weather App</h1>
+        <p>Current Temperature: <span style={temperatureStyle}>{temperature}°C</span></p>
+        <p>Conditions: {conditions}</p>
+      </div>
+    );
+  }
 }
 
-export default App
+export default App;
